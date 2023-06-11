8d5762709f24ef26fd98e1a6236e4203ad30c951
John <ironrockjohn@gmail.com>
2023-06-10 21:46:38 -0500

    Iniciando Proyecto

---
+++ src/test/java/com/alura/foroinicial/ForoInicialApplicationTests.java
@@ -0,0 +1,22 @@
+package com.alura.foroinicial;
+
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}*/
+
+@SpringBootTes
+public static void main(String[] args) {
+
+		SpringApplication.run(ApiApplication.class, args);
+		}
+
